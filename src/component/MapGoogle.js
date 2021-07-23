ad9d1f22905a2b228abc193d3faace5fd4edbf15
amapic <amaury.pichat@gmail.com>
2021-07-24 00:26:52 +0100

    gg

--- src/component/MapGoogle.js
+++ src/component/MapGoogle.js
@@ -277,6 +277,7 @@ export default function MapGoogle({ region_excluded = [], region_included = [],z
                   options = {
                     (mapProps.selectedItems.includes(parseInt(object)) || mapProps.selectedItems.includes(object)) ? mapOptionsClicked : mapOptionsNotClicked
                   }
+                  
                   />
                   // <Popover
                   //   id="mouse-over-popover"
